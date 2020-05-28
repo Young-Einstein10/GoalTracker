77fb9005fda68bc68771240be7ce324115c2868e
young-einstein10 <aabdulrahmanyusuf@gmail.com>
2020-05-28 20:32:38 +0100

    chore(tests):wrote intro unit tests

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,24 +1,62 @@
 var request = require("supertest");
+import app from "../app";
+import * as DB from "../test_db";
 
 describe("User can", () => {
-  var server;
-  beforeEach(() => {
-    server = require("../server");
+  beforeAll(() => {
+    // DB.dropAllTables();
+    // DB.createSchema();
+    DB.createAllTables();
   });
-  afterEach(() => {
-    server.close();
+
+  afterAll(() => {
+    DB.dropAllTables();
+  });
+
+  describe("POST api/v1/users/signin", () => {
+    it("should return a 200 success when user logs in with correct email and password", async () => {
+      const user = {
+        email: "john@gmail.com",
+        password: "123456",
+      };
+      const res = await request(app).post("/api/v1/users/signin").send(user);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual(jasmine.objectContaining({ status: "success" }));
+    });
   });
 
   describe("GET /", () => {
     it("should return a 200 response for getting a single gif", async () => {
-      const response = await request(server).get("/");
+      const response = await request(app).get("/");
 
       expect(response.status).toBe(200);
       expect(response.body).toEqual(
         jasmine.objectContaining({
-          info: "Node.js, Express, and Postgres API",
+          info: "Goal Tracker is live and running 🎉🎉🎉",
         })
       );
     });
   });
 });
+
+function tellJasmine(done) {
+  return function (err) {
+    if (err) {
+      done.fail(err);
+    } else {
+      done();
+    }
+  };
+}
+
+// describe("GET /", function () {
+//   it("should return a 200 response and info object", function (done) {
+//     request(app)
+//       .get("/")
+//       .set("Accept", "application/json")
+//       .expect("Content-Type", /json/)
+//       .expect(200)
+//       .end(tellJasmine(done));
+//   });
+// });
